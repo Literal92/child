344749ec325614d6f1872e7cae4b4924a77305ff
amir tavakoli <am.tavakoli1992@gmail.com>
2020-12-17 22:51:34 +0330

    klhl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDatepickerModule } from "@angular/material/datepicker";
 import { MatInputModule } from "@angular/material/input";
+import { MatSelectModule } from "@angular/material/select";
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-import {MatNativeDateModule} from '@angular/material/core';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
@@ -22,12 +23,14 @@ import {MatNativeDateModule} from '@angular/material/core';
     MatFormFieldModule,
     NoopAnimationsModule,
     MatInputModule,
+    MatSelectModule,
     MatCardModule, MatButtonModule,
     MatFormFieldModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   exports: [
-    MatCardModule, MatButtonModule, MatDatepickerModule, MatNativeDateModule, MatFormFieldModule
+    MatCardModule, MatButtonModule, MatDatepickerModule, MatNativeDateModule, MatFormFieldModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent],
